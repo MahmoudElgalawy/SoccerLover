7bfeb17e67146b101b5d104f39af6d435ce0b691
Mahmoud Elgalawy <elgalawymahmoud99@gmail.com>
2024-11-19 16:13:02 +0200

    Create Remote Service

---
+++ SoccerLover/Enums/errorEnum.swift
@@ -0,0 +1,36 @@
+//
+//  errorEnum.swift
+//  SoccerLover
+//
+//  Created by Mahmoud  on 19/11/2024.
+//
+
+import Foundation
+
+enum SoccerError : Error,CustomNSError{
+    case apiError
+    case invalidResponse
+    case invalidEndPoint
+    case noData
+    case serializationError
+    
+    var localizedDescription:String{
+        switch self{
+        case .apiError:
+            return "Failed to fetch data"
+        case .invalidResponse:
+            return "Invalid response"
+        case .invalidEndPoint:
+            return "Invalid endPoint"
+        case .noData:
+            return "No data"
+        case .serializationError:
+            return "Failed to decode data"
+        }
+    }
+    
+    var errorUserInfo :[String : Any] {
+        [NSLocalizedDescriptionKey:localizedDescription]
+    }
+}
+
--- SoccerLover/Models/CompetitionsDetails.swift
+++ SoccerLover/Models/CompetitionsDetails.swift
@@ -19,11 +19,13 @@ struct CompetitionInfo: Codable{
     let area: Area
     let code: String?
     let plan: String?
+    let emblem: String?
 }
 
 struct Match: Codable {
     let id: Int
     let utcDate: String
+    let season: Season
     let status: String
     let Matchday: Int?
     let homeTeam: Team
@@ -35,14 +37,18 @@ struct Team: Codable {
     let id: Int
     let name: String
     let shortName: String?
+    let crest: String?
 }
 
 struct Score: Codable{
+    let winner: String?
     let fullTime: MatchScore
     let halfTime: MatchScore
+    let extraTime: MatchScore?
+    let penalties: MatchScore?
 }
 
 struct MatchScore: Codable {
-    let homeTeam: Int?
-    let awayTeam: Int?
+    let home: Int?
+    let away: Int?
 }
